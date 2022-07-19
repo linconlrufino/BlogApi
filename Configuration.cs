4905a3a0ac71fdb76716d0e5e6592b63bc8f5a77
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-07-19 20:28:19 -0300

    Implementando um ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@ namespace BlogApi;
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+    public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "blog_api_IlTevUM/z0ey3NwCV/unWg==";
 }
