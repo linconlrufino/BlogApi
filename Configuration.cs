1ddfb5b2a25886811873b4c3b557e5e13b8c2f94
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-07-19 20:48:30 -0300

    Lendo do AppSettings

--- Configuration.cs
+++ Configuration.cs
@@ -5,4 +5,12 @@ public static class Configuration
     public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "blog_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
 }
